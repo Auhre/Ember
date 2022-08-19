b4e16c6e070410668940bef276de0c13e59f992d
rico <86467237+Auhre@users.noreply.github.com>
2022-08-19 14:20:10 +0800

    Update todo.js

--- todo/app/components/todo.js
+++ todo/app/components/todo.js
@@ -1,11 +1,9 @@
 import Component from '@glimmer/component';
-
-//import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 
 export default class CounterComponent extends Component {
-  //@tracked count = 0;
+  
   @service todo;
 
   @action
